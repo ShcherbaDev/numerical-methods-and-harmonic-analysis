987ca1c0c444c5853356b9846922d354abca565c
Andrii Shcherbakov <andrey.shcherbakov05@gmail.com>
2024-11-17 02:40:16 +0200

    Implement task 6

--- 1/main.py
+++ 1/main.py
@@ -18,80 +18,121 @@ def get_interpolation_range_nodes(
 
 # Вхідні дані
 range_start = 1
-range_end = 3
-number_of_nodes = 4
+range_end = 30
+min_number_of_nodes = 4
+max_number_of_nodes = 20
+number_of_nodes_step = 4
 
-# Вузли та значення функції у вузлах
-nodes_x = get_interpolation_range_nodes(range_start, range_end, number_of_nodes)
-nodes_y = [f(x) for x in nodes_x]
-nodes = list(zip(nodes_x, nodes_y))
+deltas: list[dict[int, float, float]] = []
 
-# Обрахунок точок щоб отримати гладкий та точний графік
-x_values = get_interpolation_range_nodes(range_start, range_end, 100)
-y_values = [f(x) for x in x_values]
-lagrange_values = [get_lagrange(x, nodes) for x in x_values]
+for number_of_nodes in range(min_number_of_nodes, max_number_of_nodes + 1, number_of_nodes_step):
+    # Відображати графіки тільки на першій ітерації циклу
+    is_first_iteration = number_of_nodes == min_number_of_nodes
+
+    # Вузли та значення функції у вузлах
+    nodes_x = get_interpolation_range_nodes(range_start, range_end, number_of_nodes)
+    nodes_y = [f(x) for x in nodes_x]
+    nodes = list(zip(nodes_x, nodes_y))
+
+    # Обрахунок точок щоб отримати гладкий та точний графік
+    x_values = get_interpolation_range_nodes(range_start, range_end, 1000)
+    y_values = [f(x) for x in x_values]
+    lagrange_values = [get_lagrange(x, nodes) for x in x_values]
+
+    # ============
+    #  Завдання 4
+    # ============
+
+    # Графік функції f(x) та поліному L(x)
+    if is_first_iteration:
+        plt.figure(figsize=(12, 6))
+        plt.plot(x_values, y_values, label="$f(x)$")
+        plt.plot(x_values, lagrange_values, label="$L(x)$", linestyle="--")
+        plt.scatter(nodes_x, nodes_y, label="Вузли інтерполяції", color="red", zorder=999)
+        plt.title("Інтерполяція поліномом Лагранжа функції $f(x) = \ln{(x^4 - 2x^2 + 3)}$")
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+    # Графік похибки інтерполяції f(x) - L(x)
+    # та отримання найбільшого відхилення
+    errors = [f_x - l_x for f_x, l_x in zip(y_values, lagrange_values)]
+    delta_n = max(abs(err) for err in errors)
+
+    if is_first_iteration:
+        plt.figure(figsize=(12, 6))
+        plt.plot(x_values, errors, label="Похибка $f(x) - L(x)$", color="green")
+        plt.axhline(0, color="black", linestyle=":", linewidth=3)
+        plt.title(f"Графік похибки інтерполяції $f(x) - L(x)$\nДефект наближення: {delta_n}")
+        plt.xlabel("x")
+        plt.ylabel("Похибка")
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+    # ============
+    #  Завдання 5
+    # ============
+
+    # Значення кусково-лінійної інтерполяції
+    g_values = [get_piecewise_linear_interpolation(x, nodes) for x in x_values]
+
+    # Графік f(x) та кусково-лінійної інтерполяції g(x)
+    if is_first_iteration:
+        plt.figure(figsize=(12, 6))
+        plt.plot(x_values, y_values, label="$f(x)$")
+        plt.plot(x_values, g_values, label="$g(x)$", linestyle="--")
+        plt.scatter(nodes_x, nodes_y, label="Вузли інтерполяції", color="red", zorder=999)
+        plt.title("Кусково-лінійна інтерполяція $f(x)$")
+        plt.xlabel("x")
+        plt.xlabel("y")
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+    # Графік похибки інтерполяції f(x) - g(x) 
+    # та отримання найбільшого відхилення
+    errors_g = [f(x) - g for x, g in zip(x_values, g_values)]
+    delta_g = max(abs(err) for err in errors_g)
+
+    if is_first_iteration:
+        plt.figure(figsize=(12, 6))
+        plt.plot(x_values, errors_g, label="Похибка $f(x) - g(x)$", color="green")
+        plt.axhline(0, color="black", linestyle=":", linewidth=3)
+        plt.title(f"Графік похибки інтерполяції $f(x) - g(x)$\nДефект наближення: {delta_g}")
+        plt.xlabel("x")
+        plt.ylabel("Похибка")
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+    deltas.append({
+        "nodes": number_of_nodes,
+        "delta_n": delta_n,
+        "delta_g": delta_g
+    })
 
 # ============
-#  Завдання 4
+#  Завдання 6
 # ============
 
-# Графік функції f(x) та поліному L(x)
-plt.figure(figsize=(12, 6))
-plt.plot(x_values, y_values, label="$f(x)$")
-plt.plot(x_values, lagrange_values, label="$L(x)$", linestyle="--")
-plt.scatter(nodes_x, nodes_y, label="Вузли інтерполяції", color="red", zorder=999)
-plt.title("Інтерполяція поліномом Лагранжа функції $f(x) = \ln{(x^4 - 2x^2 + 3)}$")
-plt.xlabel("x")
-plt.ylabel("y")
-plt.legend()
-plt.grid(True)
-plt.show()
+# Вивід таблиці дефектів наближення при різній кількості вузлів
+# у markdown форматі
+print("| $n$ | $\Delta_n$ | $\Delta_1$ |")
+print("| --- | --- | --- |")
+for row in deltas:
+    number_of_nodes, delta_n, delta_g = row.values()
+    print(f"| {number_of_nodes} | {delta_n} | {delta_g} |")
 
-# Графік похибки інтерполяції f(x) - L(x)
-# та отримання найбільшого відхилення
-errors = [f_x - l_x for f_x, l_x in zip(y_values, lagrange_values)]
-delta_n = max(abs(err) for err in errors)
-
-plt.figure(figsize=(12, 6))
-plt.plot(x_values, errors, label="Похибка $f(x) - L(x)$", color="green")
-plt.axhline(0, color="black", linestyle=":", linewidth=3)
-plt.title(f"Графік похибки інтерполяції $f(x) - L(x)$\nДефект наближення: {delta_n}")
-plt.xlabel("x")
-plt.ylabel("Похибка")
-plt.legend()
-plt.grid(True)
-plt.show()
-
-# ============
-#  Завдання 5
-# ============
-
-# Значення кусково-лінійної інтерполяції
-g_values = [get_piecewise_linear_interpolation(x, nodes) for x in x_values]
-
-# Графік f(x) та кусково-лінійної інтерполяції g(x)
-plt.figure(figsize=(12, 6))
-plt.plot(x_values, y_values, label="$f(x)$")
-plt.plot(x_values, g_values, label="$g(x)$", linestyle="--")
-plt.scatter(nodes_x, nodes_y, label="Вузли інтерполяції", color="red", zorder=999)
-plt.title("Кусково-лінійна інтерполяція $f(x)$")
-plt.xlabel("x")
-plt.xlabel("y")
-plt.legend()
-plt.grid(True)
-plt.show()
-
-# Графік похибки інтерполяції f(x) - g(x) 
-# та отримання найбільшого відхилення
-errors_g = [f(x) - g for x, g in zip(x_values, g_values)]
-delta_g = max(abs(err) for err in errors_g)
-
-plt.figure(figsize=(12, 6))
-plt.plot(x_values, errors_g, label="Похибка $f(x) - g(x)$", color="green")
-plt.axhline(0, color="black", linestyle=":", linewidth=3)
-plt.title(f"Графік похибки інтерполяції $f(x) - g(x)$\nДефект наближення: {delta_g}")
-plt.xlabel("x")
-plt.ylabel("Похибка")
+# Побудова графіку залежності \Delta_n і \Delta_1 від n
+plt.figure(figsize=(10, 7))
+plt.plot([row["nodes"] for row in deltas], [row["delta_n"] for row in deltas], label="$\Delta_n$", marker="o")
+plt.plot([row["nodes"] for row in deltas], [row["delta_g"] for row in deltas], label="$\Delta_1$", marker="o")
+plt.title("Залежність дефекту наближення від кількості вузлів")
+plt.xlabel("Кількість вузлів $n$")
+plt.ylabel("Дефект наближення")
 plt.legend()
 plt.grid(True)
 plt.show()
