6c2af8f89f99a6c6c090716c77593579d9e10145
Andrii Shcherbakov <andrey.shcherbakov05@gmail.com>
2024-11-28 21:28:55 +0200

    Remove computational duration column

--- 2/main.py
+++ 2/main.py
@@ -52,7 +52,7 @@ print(f"\nТаблиця для h={h}:")
 print("| x | Аналітичний розв'язок | Наближений розв'язок |")
 print("|---|-----------------------|----------------------|")
 for x, y_analytical, y_numerical in zip(x_values, analytical_solution_values, numerical_function_values):
-    print(f"| {x} | {round(y_analytical, 4)} | {round(y_numerical, 4)} |")
+    print(f"| {x} | {round(y_analytical, 8)} | {round(y_numerical, 8)} |")
 print("")
 
 # ============
@@ -64,18 +64,15 @@ step_sizes = [0.1, 0.05, 0.025, 0.01]
 results = []
 
 for h in step_sizes:
-    timestamp = time.perf_counter()
     _, numerical_function_values = runge_kutta_2_order(differential_function, x_0, y_0, x_n, h)
     numerical_function_value_at_xn = numerical_function_values[-1]
     error = abs(analytical_function_value_at_xn - numerical_function_value_at_xn)
-    computation_duration = time.perf_counter() - timestamp
-    results.append((h, numerical_function_value_at_xn, error, computation_duration))
+    results.append((h, numerical_function_value_at_xn, error))
 
-print("| h | Наближений розв'язок у x_n | Похибка | Час обчислення |")
-print("|---|----------------------------|---------|----------------|")
-for h, y_numerical, error, computation_duration in results:
-    print(f"| {h} | {round(y_numerical, 4)} | {round(error, 4)} | {round(computation_duration, 6)} |")
+print("| h | Наближений розв'язок у x_n | Похибка |")
+print("|---|----------------------------|---------|")
+for h, y_numerical, error in results:
+    print(f"| {h} | {round(y_numerical, 8)} | {round(error, 8)} |")
 
 print("Висновок: при зменшенні кроку зменшується й похибка")
 print("При h=0.01 похибка взагалі мінімальна, що може вказувати на високу точність даного рівняння")
-print("Але також й зі зменшенням кроку, трохи збільшується й час обчислення")
