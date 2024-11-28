0a700c697d682ec83a61dd9f9a75609f70fe10ca
Andrii Shcherbakov <andrey.shcherbakov05@gmail.com>
2024-11-28 23:15:53 +0200

    Implement task 6

--- 2/main.py
+++ 2/main.py
@@ -45,61 +45,95 @@ def differential_function(x, y):
 x_values = arrange(x_0, x_n, h)
 analytical_solution_values = [analytical_function(x, C_value) for x in x_values]
 
-# Розрахунок наближеного розв'язку за Рунге-Кутти 2-го розряду
-_, numerical_function_values = runge_kutta_2_order(differential_function, x_0, y_0, x_n, h)
+# Розрахунок наближеного розв'язку за Рунге-Кутти 2-го і 4-го розрядів
+_, numerical_function_values_2_order = runge_kutta_2_order(differential_function, x_0, y_0, x_n, h)
+_, numerical_function_values_4_order = runge_kutta_4_order(differential_function, x_0, y_0, x_n, h)
 
 print(f"\nТаблиця для h={h}:")
-print("| x | Аналітичний розв'язок | Наближений розв'язок |")
+print("| x | Аналітичний розв'язок | Наближений розв'язок Рунге-Кутти 2-го порядку | Наближений розв'язок Рунге-Кутти 4-го порядку |")
 print("|---|-----------------------|----------------------|")
-for x, y_analytical, y_numerical in zip(x_values, analytical_solution_values, numerical_function_values):
-    print(f"| {x} | {round(y_analytical, 8)} | {round(y_numerical, 8)} |")
+for x, y_analytical, y_numerical_2_order, y_numerical_4_order in zip(x_values, analytical_solution_values, numerical_function_values_2_order, numerical_function_values_4_order):
+    print(f"| {x} | {round(y_analytical, 8)} | {round(y_numerical_2_order, 8)} | {round(y_numerical_4_order, 8)} |")
 print("")
 
 # ============
 #  Завдання 4
 # ============
 
-# Потрібно для 5-го завдання
 def theoretical_error_runge_kutta_2_order(h):
     return h ** 2
+def theoretical_error_runge_kutta_4_order(h):
+    return h ** 4
 
 analytical_function_value_at_xn = analytical_function(x_n, C_value)
 step_sizes = [0.1, 0.05, 0.025, 0.01]
-results = []
 
-actual_errors = []
-theoretical_errors = []
+runge_kutta_2_results = []
+runge_kutta_2_actual_errors = []
+runge_kutta_2_theoretical_errors = []
+
+runge_kutta_4_results = []
+runge_kutta_4_actual_errors = []
+runge_kutta_4_theoretical_errors = []
 
 for h in step_sizes:
-    _, numerical_function_values = runge_kutta_2_order(differential_function, x_0, y_0, x_n, h)
-    numerical_function_value_at_xn = numerical_function_values[-1]
+    _, runge_kutta_2_numerical_function_values = runge_kutta_2_order(differential_function, x_0, y_0, x_n, h)
+    numerical_function_value_at_xn = runge_kutta_2_numerical_function_values[-1]
     
-    actual_error = abs(analytical_function_value_at_xn - numerical_function_value_at_xn)
-    actual_errors.append(actual_error)
+    runge_kutta_2_actual_error = abs(analytical_function_value_at_xn - numerical_function_value_at_xn)
+    runge_kutta_2_actual_errors.append(runge_kutta_2_actual_error)
     
-    theoretical_error = theoretical_error_runge_kutta_2_order(h)
-    theoretical_errors.append(theoretical_error)
+    runge_kutta_2_theoretical_error = theoretical_error_runge_kutta_2_order(h)
+    runge_kutta_2_theoretical_errors.append(runge_kutta_2_theoretical_error)
     
-    results.append((h, numerical_function_value_at_xn, actual_error))
+    runge_kutta_2_results.append((h, numerical_function_value_at_xn, runge_kutta_2_actual_error))
+    
+    
+    _, runge_kutta_4_numerical_function_values = runge_kutta_4_order(differential_function, x_0, y_0, x_n, h)
+    numerical_function_value_at_xn = runge_kutta_4_numerical_function_values[-1]
+    
+    runge_kutta_4_actual_error = abs(analytical_function_value_at_xn - numerical_function_value_at_xn)
+    runge_kutta_4_actual_errors.append(runge_kutta_4_actual_error)
+    
+    runge_kutta_4_theoretical_error = theoretical_error_runge_kutta_4_order(h)
+    runge_kutta_4_theoretical_errors.append(runge_kutta_4_theoretical_error)
+    
+    runge_kutta_4_results.append((h, numerical_function_value_at_xn, runge_kutta_4_actual_error))
 
-print("| h | Наближений розв'язок у x_n | Похибка |")
-print("|---|----------------------------|---------|")
-for h, y_numerical, error in results:
+print("| h | Наближений розв'язок у x_n для Рунге-Кутти 2-го порядку | Похибка |")
+print("|---|---------------------------------------------------------|---------|")
+for h, y_numerical, error in runge_kutta_2_results:
     print(f"| {h} | {round(y_numerical, 8)} | {round(error, 8)} |")
 
 print("Висновок: при зменшенні кроку зменшується й похибка")
-print("При h=0.01 похибка дуже мала, що може вказувати на високу точність даного диференціального рівняння")
+print("При h=0.01 похибка дуже мала, що може вказувати на високу точність даного диференціального рівняння\n")
+
+print("| h | Наближений розв'язок у x_n для Рунге-Кутти 4-го порядку | Похибка |")
+print("|---|---------------------------------------------------------|---------|")
+for h, y_numerical, error in runge_kutta_4_results:
+    print(f"| {h} | {round(y_numerical, 8)} | {round(error, 8)} |")
+print("Висновок: Рунге-Кутти 4-го порядку має меншу похибку, ніж другого порядку")
 
 # ============
 #  Завдання 5
 # ============
 
 plt.figure(figsize=(12, 6))
-plt.plot(step_sizes, actual_errors, label="Фактична похибка", marker="o")
-plt.plot(step_sizes, theoretical_errors, label="Теоретична похибка", marker="o")
+plt.plot(step_sizes, runge_kutta_2_actual_errors, label="Фактична похибка", marker="o")
+plt.plot(step_sizes, runge_kutta_2_theoretical_errors, label="Теоретична похибка", marker="o")
 plt.title("Фактична і теоретична похибки Рунге-Кутти 2-го порядку")
 plt.xlabel("Крок h")
-plt.xlabel("Похибка")
+plt.ylabel("Похибка")
+plt.legend()
+plt.grid(True)
+plt.show()
+
+plt.figure(figsize=(12, 6))
+plt.plot(step_sizes, runge_kutta_4_actual_errors, label="Фактична похибка", marker="o")
+plt.plot(step_sizes, runge_kutta_4_theoretical_errors, label="Теоретична похибка", marker="o")
+plt.title("Фактична і теоретична похибки Рунге-Кутти 4-го порядку")
+plt.xlabel("Крок h")
+plt.ylabel("Похибка")
 plt.legend()
 plt.grid(True)
 plt.show()
--- 2/runge_kutta.py
+++ 2/runge_kutta.py
@@ -1,7 +1,6 @@
 from arrange import arrange
 
-
-def runge_kutta_2_order(f, x_0, y_0, x_n, h) -> tuple[list[int], list[int]]:
+def runge_kutta_2_order(f, x_0, y_0, x_n, h) -> tuple[list[float], list[float]]:
     x_values = arrange(x_0, x_n, h)
     y_values = [0] * len(x_values)
     y_values[0] = y_0
@@ -14,5 +13,23 @@ def runge_kutta_2_order(f, x_0, y_0, x_n, h) -> tuple[list[int], list[int]]:
         k_2 = h * f(x + h, y + k_1)
 
         y_values[i] = y + (k_1 + k_2) / 2
+
+    return x_values, y_values
+
+def runge_kutta_4_order(f, x_0, y_0, x_n, h) -> tuple[list[float], list[float]]:
+    x_values = arrange(x_0, x_n, h)
+    y_values = [0] * len(x_values)
+    y_values[0] = y_0
+
+    for i in range(1, len(x_values)):
+        x = x_values[i - 1]
+        y = y_values[i - 1]
+
+        k_1 = h * f(x, y)
+        k_2 = h * f(x + h / 2, y + k_1 / 2)
+        k_3 = h * f(x + h / 2, y + k_2 / 2)
+        k_4 = h * f(x + h, y + k_3)
+
+        y_values[i] = y + (k_1 + 2 * k_2 + 2 * k_3 + k_4) / 6
 
     return x_values, y_values
